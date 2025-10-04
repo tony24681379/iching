9ed65a735945f99073af39fd3a58c5414fe296ee
Tony Huang <tony_huang@vicone.com>
2025-10-05 00:03:43 +0800

    feat: update help

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -6,11 +6,12 @@ const Help: React.FC = () => {
     <div className="iching-help">
       <strong>使用說明</strong>
       <div className="iching-help-lines">
-        <div>每個卦象由6個爻組成（從下到上：第1爻到第6爻）</div>
         <div>點擊線條可以切換陰爻（斷線）和陽爻（實線）</div>
         <div>點擊右側圓點可以設定變爻（虛線圓=不變，實心圓=變爻）</div>
-        <div>變爻會使該爻的陰陽顛倒，形成變卦</div>
-        <div>共有 64 種可能的卦象組合</div>
+        <div>
+          「隨機起卦」功能模擬傳統卜卦方式，由下至上（初爻至上爻）逐爻生成
+        </div>
+        <div>點擊右上角的設定圖示可以顯示或隱藏白話易經的頁碼</div>
       </div>
     </div>
   );
